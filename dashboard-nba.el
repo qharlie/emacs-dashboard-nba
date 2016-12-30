7a9140740f43f9f2cf41b73e365447144fb86a9b
qorrect <charlie.fats@gmail.com>
2016-12-29 18:54:15 -0600

    Oops, messed up the screenshot

--- dashboard-nba.el
+++ dashboard-nba.el
@@ -9,21 +9,78 @@
   (when (car list)
 
     (insert title )
+    (insert "\n\n\t")
     (mapc (lambda (el)
 	    (setq url (nth 1 (split-string el "__")) )
-	    (setq title (nth 0 (split-string el "__")) )
-            (insert "\n    ")
+	    (setq desc (nth 0 (split-string el "__")) )
+	    (insert desc "\n\n\t\t")
+
             (widget-create 'push-button
                            :action `(lambda (&rest ignore)
 				      (browse-url ,url))
                            :mouse-face 'highlight
                            :follow-link "\C-m"
-                           :button-prefix ""
-                           :button-suffix ""
+                           :button-prefix "["
+                           :button-suffix "]"
                            :format "%[%t%]"	    
-			   title
-			   ))
+			   "Watch Highlights"
+			   )
+	    (insert "\n")
+	    )
+
           list)))
+
+
+(defun dashboard-read-nba-file (file-path)
+  (setq nba-list (mapcar (lambda (entry)
+			   (format "\n\t%s\n\t\t%s (%s/%s) : %s\n\t\t%s (%s/%s) : %s__https://duckduckgo.com/?q=%s+vs+%s+highlights+%s&ia=videos"
+				   (let-alist entry .nugget.text)
+					 
+				   (let-alist entry .vTeam.triCode)
+				   (let-alist entry .vTeam.win)
+				   (let-alist entry .vTeam.loss)
+				   (let-alist entry .vTeam.score)
+
+				   (let-alist entry .hTeam.triCode)
+				   (let-alist entry .hTeam.win)
+				   (let-alist entry .hTeam.loss)
+				   (let-alist entry .hTeam.score)
+				   
+				   (let-alist entry .vTeam.triCode)
+				   (let-alist entry .hTeam.triCode)
+				   (format-time-string "%Y-%m-%d")
+				   ))
+        				;(concat (let-alist entry .data.title ) (concat " - " (let-alist entry .data.url ))))
+			 (let-alist (json-read-file  file-path) .games)))
+  )
+
+
+(defun dashboard-highlight-nba ()
+  (interactive)
+  (setq file-path "/tmp/dashboard_nba.json")
+  (setq nba-list (mapcar (lambda (entry)
+			   (progn
+			     (highlight-phrase (let-alist entry .nugget.text) "hi-blue-b")
+			     (setq hteam-win (> (string-to-int (let-alist entry .hTeam.score))  (string-to-int (let-alist entry .vTeam.score))))
+			     (if hteam-win
+				 (progn
+				   (highlight-phrase (let-alist entry .hTeam.triCode) "hi-green")
+				   (highlight-phrase (let-alist entry .vTeam.triCode) "hi-red-b")
+				   )
+			       (highlight-phrase (let-alist entry .vTeam.triCode) "hi-green")
+			       (highlight-phrase (let-alist entry .hTeam.triCode) "hi-red-b")			       
+			       (message "AWAY TEAM")
+			       )
+
+			     
+			   ))
+			 (let-alist (json-read-file  file-path) .games)))
+			 
+	
+  
+  
+  )
+
 
 (defun dashboard-insert-nba (list-size)
   "Add the list of LIST-SIZE items from recently edited files."
@@ -38,35 +95,22 @@
 
 	(url-copy-file (format "http://data.nba.net/data/10s/prod/v1/%s/scoreboard.json" (format-time-string "%Y%m%d" (time-subtract (current-time) (days-to-time 1))))  file-path)
 	
-	(setq nba-list (mapcar (lambda (entry)
-				 (format "\n\t%s\n\t\t%s (%s/%s) : %s\n\t\t%s (%s/%s) : %s__https://duckduckgo.com/?q=NBA+%s+vs+%s+highlights+%s&ia=videos"
-					 (let-alist entry .nugget.text)
-					 
-					 (let-alist entry .vTeam.triCode)
-					 (let-alist entry .vTeam.win)
-					 (let-alist entry .vTeam.loss)
-					 (let-alist entry .vTeam.score)
-
-					 (let-alist entry .hTeam.triCode)
-					 (let-alist entry .hTeam.win)
-					 (let-alist entry .hTeam.loss)
-					 (let-alist entry .hTeam.score)
-
-					 (let-alist entry .vTeam.triCode)
-					 (let-alist entry .hTeam.triCode)
-					 (format-time-string "%Y-%m-%d")
-					 ))
-        				;(concat (let-alist entry .data.title ) (concat " - " (let-alist entry .data.url ))))
-			       (let-alist (json-read-file  file-path) .games)))
 
 
+	(setq nba-list (dashboard-read-nba-file file-path))
 	
 	(when (dashboard-insert-nba-list
 	       "Last nights NBA scores:"
 	       (dashboard-subseq nba-list 0 list-size)))	 
 	(dashboard-insert--shortcut "q" "NBA Scores:")
+
+	(dashboard-highlight-nba)
+	
 	)
-    
+
+
+
+
     ))
 
 					;(time-subtract
